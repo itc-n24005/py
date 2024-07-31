b73d6a186096c2ac280251b2d80c2d7641066406
itc-n24005 <n24005@std.it-college.ac.jp>
2024-07-31 06:26:23 +0000

    Q5_10_1.py

---
+++ Q5_10_1.py
@@ -0,0 +1,7 @@
+def is_number_of_positions_of_10(x):
+    return 10 <= x < 20
+
+print(is_number_of_positions_of_10(15))  # 出力: True
+print(is_number_of_positions_of_10(9))   # 出力: False
+print(is_number_of_positions_of_10(20))  # 出力: False
+
